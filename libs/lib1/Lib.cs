34454c45252870be04d6e646b68488ea3ce1a6d0
Phillip Carter <phillip.carter@outlook.com>
2015-11-04 09:40:34 -0800

    Change Lib.cs to use async/await

--- libs/lib1/Lib.cs
+++ libs/lib1/Lib.cs
@@ -5,6 +5,7 @@ using System.Net;
 #else
 // Generic collections are just a part of
 using System.Net.Http;
+//using System.Threading.Tasks;
 #endif
 
 using System.Text.RegularExpressions;
@@ -18,20 +19,30 @@ namespace Lib
 #else
         private readonly HttpClient _client = new HttpClient();
 #endif
-        public string Whee()
+
+#if NET40
+        public string GetDotNetCount()
         {
             string url = "http://www.dotnetfoundation.org/";
-            
-#if NET40
+          
             var uri = new Uri(url);
             var result = _client.DownloadString(uri);
-#else
-            var result = _client.GetStringAsync(url).Result;
-#endif
             
             int dotNetCount = Regex.Matches(result, ".NET").Count;
             
             return $"Dotnet Foundation mentions .NET {dotNetCount} times!";
         }
+#else
+        public string GetDotNetCountAsync()
+        {
+            string url = "http://www.dotnetfoundation.org/";
+            
+            var result = await _client.GetStringAsync(url);
+            
+            int dotNetCount = Regex.Matches(result, ".NET").Count;
+            
+            return $"Dotnet Foundation mentions .NET {dotNetCount} times!";
+        }
+#endif
     }
 }
