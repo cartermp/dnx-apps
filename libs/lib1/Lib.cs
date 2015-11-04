e5ab521d3482dc2fe2c9012e6272ed676fa4a56b
cartermp <phillip.carter@outlook.com>
2015-11-04 09:17:52 -0800

    Cross-compiling for .NET Core and .NET 4

--- libs/lib1/Lib.cs
+++ libs/lib1/Lib.cs
@@ -1,9 +1,37 @@
 using System;
 
+#if NET40
+using System.Net;
+#else
+// Generic collections are just a part of
+using System.Net.Http;
+#endif
+
+using System.Text.RegularExpressions;
+
 namespace Lib
 {
     public class Foo
     {
-        public string Bar => "bananas!";
+#if NET40
+        private readonly WebClient _client = new WebClient();
+#else
+        private readonly HttpClient _client = new HttpClient();
+#endif
+        public string Whee()
+        {
+            string url = "http://www.dotnetfoundation.org/";
+            
+#if NET40
+            var uri = new Uri(url);
+            var result = _client.DownloadString(uri);
+#else
+            var result = _client.GetStringAsync(url).Result;
+#endif
+            
+            int dotNetCount = Regex.Matches(result, ".NET").Count;
+            
+            return $"Dotnet Foundation mentions .NET {dotNetCount} times!";
+        }
     }
 }
